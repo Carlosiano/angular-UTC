81e58fc9dc674dd7eab5d8b3e17482710c7c568a
Rolisnani21 <Carlosiano>
2024-07-21 01:37:03 +0700

    commit aja

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'angular-UTC-2' title`, () => {
+  it(`should have the 'angular-UTC' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-UTC-2');
+    expect(app.title).toEqual('angular-UTC');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, angular-UTC-2');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, angular-UTC');
   });
 });
